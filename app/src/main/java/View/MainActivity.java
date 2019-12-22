f3d4d28cb789cef63839e4955968e1922540b4f7
Anael Gorfinkel <anaelgor@post.bgu.ac.il>
2019-12-22 18:37:00 +0200

    permissions feature. WRITE YOUR PERMISSIONS OIN THE MANIFEST FILE ONLY

--- app/src/main/java/View/MainActivity.java
+++ app/src/main/java/View/MainActivity.java
@@ -1,13 +1,18 @@
 package View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -16,6 +21,7 @@ import com.example.modamedicandroidapplication.R;
 import java.util.Calendar;
 
 import Model.NotificationOfMichal;
+import Model.Permissions;
 
 /*
 Home page screen
@@ -25,11 +31,23 @@ public class MainActivity extends AppCompatActivity {
     AlarmManager alarmManager = null;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * PERMMISIONS REQUEST
+         * ALL YOU NEED TO DO IS TO INSERT THE PERMISSION NAME TO THE MANIFEST FILE
+         */
+        Permissions permissions = new Permissions(this);
+        try {
+            permissions.requestPermissions();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        //end permissions requests
+
         String text = "יאללה כנס יא טמבל";
         notifications_init();
         notifications(MainActivity.class,text);
