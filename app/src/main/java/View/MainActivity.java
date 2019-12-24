31c8a0137a40a0275a697d9a5d5f006d0a6beade
maore <maorelfassy@gmail.com>
2019-12-24 18:44:15 +0200

    sign in page & home page are ready

--- app/src/main/java/View/MainActivity.java
+++ app/src/main/java/View/MainActivity.java
@@ -8,6 +8,9 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.modamedicandroidapplication.R;
 
@@ -16,6 +19,8 @@ import java.util.Calendar;
 import Model.DailyNotification;
 import Model.PeriodicNotification;
 
+import static View.BindingValues.LOGGED_USERNAME;
+
 /*
 Home page screen
  */
@@ -23,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
     //todo: this should be moved to controoler
     AlarmManager alarmManager = null;
 
-
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //todo: move this to controller. also check what happen if user open the app again,
-    // probably this should be implemented from getInstance
+    // probably this should be implemented from getInstance. this should be written from
+    // HomePageActivity, because only there we have the logged in user.
     private void setNotifcations() {
         if (alarmManager == null)
             alarmManager = (AlarmManager)(this.getSystemService( Context.ALARM_SERVICE ));
@@ -62,4 +69,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //todo: implements this
+    public void loginFunction(View view) {
+        Log.i("Main Page","Login button clicked");
+        EditText username_textfield = findViewById(R.id.username_textfield);
+        EditText password_textfield = findViewById(R.id.password_textfield);
+
+        this.username = username_textfield.getText().toString();
+        this.password = password_textfield.getText().toString();
+
+        //todo: change this the user REAL name from db. "a" and "a" only for checks
+        Log.i("Main Page", "User " + username + " with password " + password + " logged in");
+        if (username.equals("a") && password.equals("a")) {
+            Intent intent = new Intent(this, HomePageActivity.class);
+            intent.putExtra(BindingValues.LOGGED_USERNAME, username);
+            startActivity(intent);
+        }
+
+    }
+    //todo: implements this
+
+    public void forgetPasswordFunction(View view) {
+        Log.i("Main Page","Forgot password button clicked");
+
+    }
 }
