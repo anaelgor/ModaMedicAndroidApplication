ef57e6870790b8d6b1b9ff1457e2c81ff75aeed4
Anael Gorfinkel <anaelgor@post.bgu.ac.il>
2020-04-10 12:42:14 +0300

    extract weather by broadcast receiver

--- app/src/main/java/Model/Metrics/LocationBroadcastReceiver.java
+++ app/src/main/java/Model/Metrics/LocationBroadcastReceiver.java
@@ -1,25 +1,32 @@
 package Model.Metrics;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.location.LocationManager;
 import android.util.Log;
-
-import Model.Utils.Constants;
 
 public class LocationBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "LocationBroadcast";
+    LocationManager locationManager;
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.sharedPreferencesName,Context.MODE_PRIVATE);
-
         //todo: do your metric job here and save it on sharedPref by using this:
         // sharedPref.edit().putString("location",location).apply();
 
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
-        Log.i(TAG,"just for debugging");
+        Weather weather = new Weather(this.locationManager, context);
+
+        long start = System.currentTimeMillis();
+
+        while (System.currentTimeMillis() < start + 10000); //wait 10 seconds until location is updating
+
+        Log.i(TAG,"Location tracker via broadcast");
 
     }
 }
