35350fa106279daa51c7711c1cb620b60c9d5bd1
maore <maorelfassy@gmail.com>
2020-05-02 18:35:46 +0300

    NOTIFICATIONS ARE WORK!#@@#@!#!@ THANKS ANAEL

--- app/src/main/java/Model/Notifications/NotificationsManager.java
+++ app/src/main/java/Model/Notifications/NotificationsManager.java
@@ -10,7 +10,6 @@ import android.os.Build;
 import android.util.Log;
 
 import java.util.Calendar;
-import java.util.Random;
 
 import Model.Utils.Configurations;
 
@@ -21,10 +20,14 @@ public class NotificationsManager {
     AlarmManager alarmManager;
     Context context;
     private static final String TAG = "NotificationsManager";
+
+
     public NotificationsManager(Context context) {
         this.context = context;
     }
 
+    //todo: maybe this should be written only after HomePageActivity, because only there we have the logged in user.
+    //todo: also, add an option to configure the time by a configurations file
     public void setNotifications() {
         createNotificationChannel("MainChannel");
         if (alarmManager == null)
@@ -34,30 +37,27 @@ public class NotificationsManager {
         int daily_hour = Configurations.getNotificationHour(context,"daily");
         int periodic_minute = Configurations.getNotificationMinute(context,"periodic");
         int periodic_hour =  Configurations.getNotificationHour(context,"periodic");
-        //Daily notification
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, daily_hour);
-        calendar.set(Calendar.MINUTE, daily_minute);
-        long dailyTime = calendar.getTimeInMillis() + randomTime();
-     //   dailyTime = Calendar.getInstance().getTimeInMillis() +20000;
 
+        //Daily notification - one in 16:00 and one in 19:00
+        Calendar daily_calendar = Calendar.getInstance();
+        daily_calendar.setTimeInMillis(System.currentTimeMillis());
+        daily_calendar.set(Calendar.HOUR_OF_DAY, daily_hour);
+        daily_calendar.set(Calendar.MINUTE, daily_minute);
+
+        Calendar periodic_calendar = Calendar.getInstance();
+        periodic_calendar.setTimeInMillis(System.currentTimeMillis());
+        periodic_calendar.set(Calendar.HOUR_OF_DAY, periodic_hour);
+        periodic_calendar.set(Calendar.MINUTE, periodic_minute);
+
+        setRepeatingNotification(DailyNotification.class, daily_calendar.getTimeInMillis() + randomTime(), AlarmManager.INTERVAL_DAY);
         //Periodic notification
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.setTimeInMillis(System.currentTimeMillis());
-        calendar2.set(Calendar.HOUR_OF_DAY, periodic_hour);
-        calendar2.set(Calendar.MINUTE, periodic_minute);
-        long periodicTime = calendar2.getTimeInMillis() + randomTime();
-     //   periodicTime = Calendar.getInstance().getTimeInMillis() + 22000;
-
-        setRepeatingNotification(DailyNotification.class, dailyTime , AlarmManager.INTERVAL_DAY);
-        setRepeatingNotification(PeriodicNotification.class, periodicTime, AlarmManager.INTERVAL_DAY);
+        setRepeatingNotification(PeriodicNotification.class, periodic_calendar.getTimeInMillis() + randomTime(), AlarmManager.INTERVAL_DAY);
     }
 
     private void setRepeatingNotification(Class notification_class, long time, long interval) {
         Intent intent = new Intent(context, notification_class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, time, interval, pendingIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, interval, pendingIntent);
         Log.i(TAG,String.format("notification %s has been set to %s", notification_class.toString(), time));
     }
 
@@ -66,8 +66,8 @@ public class NotificationsManager {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "MainChannel";
-            String description = "MainChannel";
+            CharSequence name = "MainChanneel";
+            String description = "MainChanneel";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
